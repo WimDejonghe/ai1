4246f193fcf20d70e0b8bfeef66fc28f646276c2
Wim Dejonghe <wim.dejonghe@vives.be>
2023-07-24 21:42:55 +0200

    commit70

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -63,5 +63,14 @@ module.exports = {
   plugins: [
     '@vuepress/plugin-back-to-top',
     '@vuepress/plugin-medium-zoom',
+    [
+      'vuepress-plugin-mathjax',
+      {
+        target: 'svg',
+        macros: {
+          '*': '\\times',
+        },
+      },
+    ],
   ]
 }
